2234673199d591afd2c9a8e7cc87481e01dbab78
sverre <sverre.hamre@gmail.com>
2020-07-19 21:12:11 +0200

    Update build/simulate script, support for libraries.

--- run_tb.cmd
+++ run_tb.cmd
@@ -1,9 +1,28 @@
 @echo off
-set file=%1
+set filename=%1
+set folder=%2
+set task=%3
 
-For %%A in ("%file%") do (
-    set name=%%~nA
+for /F "tokens=2,3 delims=\" %%A in ("%folder%") do (
+    set f1="%%A"
+    set f2="%%B"
+) 
+
+set lib="work"
+if %f1% == "lib" (
+    set lib=%f2%
 )
+
+if %task% == 1 (
+    goto run_tb
+)
+
 @echo on
-ghdl -e %name%
-ghdl -r %name%
+ghdl -a --std=08 --work=%lib% %filename%
+@echo off
+goto:eof
+
+:run_tb
+@echo on
+ghdl -e --std=08 --work=%lib% %filename%
+ghdl -r --std=08 --work=%lib% %filename% --vcd=wave.vcd
